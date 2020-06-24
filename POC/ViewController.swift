a7c42bd7259750b068186bdb46a46c14ec1a98e1
PhChaithra <phchaithra1@gmail.com>
2020-06-25 00:28:08 +0530

    Initial Commit with pod files configured for Alamofire and SwiftyJSON framework pods

---
+++ POC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  POC
+//
+//  Created by ChaithraPH on 25/06/20.
+//  Copyright © 2020 ChaithraPH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
