952f93110d988e1dba6bc70eb56638c14e54bb65
PhChaithra <phchaithra1@gmail.com>
2020-06-25 23:44:42 +0530

    [Files added] Model files

---
+++ POC/Model/Content.swift
@@ -0,0 +1,26 @@
+//
+//  Content.swift
+//  POC
+//
+//  Created by ChaithraPH on 25/06/20.
+//  Copyright © 2020 ChaithraPH. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Content {
+    let title:String
+    let imageURL:String
+    let description:String
+    
+    init(title:String,imageURL:String,description:String)
+    {
+        self.title = title
+        self.imageURL = imageURL
+        self.description = description
+    }
+    
+}
+
+
